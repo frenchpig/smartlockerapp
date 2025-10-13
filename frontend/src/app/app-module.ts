8b3a2a1e7b3db2f93640c2980e4eea55d7a791c8
frenchpig <auspont.javier@gmail.com>
2025-10-13 01:30:58 -0300

    Revert "Merge remote-tracking branch 'origin/frontend' into dev-javier"
    This reverts commit 98cf05ef4a3df99ead8cf082e4ee26488a72ea22, reversing
    changes made to f8b4c8709b16852c55fd1e8763528b05d1671ca0.

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -6,32 +6,20 @@ import { App } from './app';
 import { TestBackend } from './features/test-backend/test-backend';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/auth/auth-interceptor';
-<<<<<<< HEAD
-=======
-
-import { Home } from './features/cliente/home/home';
-import { PedidoClave } from './features/cliente/pedido-clave/pedido-clave';
-import { PedidoQr } from './features/cliente/pedido-qr/pedido-qr';
->>>>>>> origin/frontend
 
 @NgModule({
-  declarations: [App, TestBackend,],
-
+  declarations: [
+    App,
+    TestBackend
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    Home,
-    PedidoClave,
-    PedidoQr
+    AppRoutingModule
   ],
-
   providers: [
-    provideHttpClient(withInterceptors([authInterceptor])),
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
   ],
-
   bootstrap: [App]
 })
-
 export class AppModule { }
--- frontend/src/app/core/auth/auth-guard.ts
+++ frontend/src/app/core/auth/auth-guard.ts
@@ -3,7 +3,6 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth';
 
 export const authGuard: CanActivateFn = () => {
-<<<<<<< HEAD
   const auth = inject(AuthService);
   const router = inject(Router);
 
@@ -11,13 +10,4 @@ export const authGuard: CanActivateFn = () => {
 
   router.navigate(['/login']);
   return false;
-=======
-  // const auth = inject(AuthService);
-  const router = inject(Router);
-
-  // if (auth.isAuthenticated()) return true;
-
-  // router.navigate(['/login']);
-  return true; //false
->>>>>>> origin/frontend
 };
